cd8eff043c58facef5d41b4d61c41aa958b790ab
Petros <petrosd1911@hotmail.com>
2026-02-27 15:22:19 +0100

    fix ts crash

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -4,9 +4,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
 
+let _prisma: any;
+
 if (!connectionString) {
   console.warn("DATABASE_URL is not set. Prisma client will be disabled.");
-  // Export a proxy that throws a descriptive error when any property is accessed.
   const handler: ProxyHandler<any> = {
     get() {
       const err: any = new Error("Database not configured (DATABASE_URL missing)");
@@ -14,10 +15,10 @@ if (!connectionString) {
       throw err;
     },
   };
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore - export a proxy in place of PrismaClient when DB is missing
-  export const prisma = new Proxy({}, handler);
+  _prisma = new Proxy({}, handler);
 } else {
   const adapter = new PrismaPg({ connectionString });
-  export const prisma = new PrismaClient({ adapter });
+  _prisma = new PrismaClient({ adapter });
 }
+
+export const prisma = _prisma as any;
